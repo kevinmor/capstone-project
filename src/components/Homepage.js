97df52cc73730d8332008ce404d0e93a0762fd71
kevinmor <kevmor623@gmail.com>
2023-05-10 02:20:18 -0700

    updated homepage to include all required sections minus and cards and unstyled

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,7 @@
 import chef from '../img/rcb.jpg'
 
-const Homepage = () => {
-    // const homepageContainer = {
-    //     display: "flex",
-    //     justifyContent: "center"
-    // }
-    return(
+const Hero = () => {
+    return (
         <div className='greenBg'>
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
@@ -18,5 +14,51 @@ const Homepage = () => {
         </div>
     )
 }
+
+const Specials = () => {
+    return (
+        <section>
+            <h1>This weeks Specials!</h1>
+            {/* Update button for accessibility */}
+            <button>Online Menu</button>
+            <p>Insert 3 cards with specials here</p>
+        </section>
+    )
+}
+
+const Testimonials = () => {
+    return (
+        <section className='greenBg'>
+            <h2>Testimonials</h2>
+            <p>Insert 3-4 cards with testimonials here based on figma design</p>
+        </section>
+    )
+}
+
+const About = () => {
+    return (
+        <section>
+            <h1>About Section</h1>
+            <p>Insert text here</p>
+        </section>
+    )
+}
+
+const Homepage = () => {
+    // const homepageContainer = {
+    //     display: "flex",
+    //     justifyContent: "center"
+    // }
+    return(
+        <>
+            <Hero />
+            <Specials />
+            <Testimonials />
+            <About />
+        </>
+    )
+}
+
+
 
 export default Homepage
